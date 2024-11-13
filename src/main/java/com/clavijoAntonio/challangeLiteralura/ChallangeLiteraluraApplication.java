b65019563881b8f3a73420442b669893d1b4fb20
antonio clavijo <clavijo1785@gmail.com>
2024-11-12 21:36:50 -0500

    se agrega proyecto a github

---
+++ src/main/java/com/clavijoAntonio/challangeLiteralura/ChallangeLiteraluraApplication.java
@@ -0,0 +1,13 @@
+package com.clavijoAntonio.challangeLiteralura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallangeLiteraluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallangeLiteraluraApplication.class, args);
+	}
+
+}
